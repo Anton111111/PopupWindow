709c1cd66d767dbe33826754712bbbc758d02bbe
Anton Potekhin <anton.potekhin@orbitsoft.com>
2021-06-07 16:26:37 +0300

    new

--- app/src/main/java/com/anton111111/popupwindow/RowView.kt
+++ app/src/main/java/com/anton111111/popupwindow/RowView.kt
@@ -12,6 +12,7 @@ import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import android.widget.ScrollView
+import androidx.core.content.ContextCompat
 import com.anton111111.popupwindow.databinding.RowViewBinding
 
 @SuppressLint("ViewConstructor")
@@ -41,12 +42,11 @@ class RowView(
                 val listView = createList(this, items).apply {
                     measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
                 }
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    //If the inflated view doesn't have a background set, or the popup window
-                    // itself doesn't have a background set (or has a transparent background)
-                    //then you won't get a shadow.
-                    setBackgroundDrawable(ColorDrawable(Color.WHITE))
+                //If the inflated view doesn't have a background set, or the popup window
+                // itself doesn't have a background set (or has a transparent background)
+                //then you won't get a shadow.
+                setBackgroundDrawable(ColorDrawable(Color.WHITE))
+                if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
                     elevation = 4.toPx.toFloat()
                 }
                 //To close on touch outside
@@ -61,16 +61,12 @@ class RowView(
 
     private fun createList(popup: PopupWindow, popupItems: List<String>) =
         ScrollView(context).apply {
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                setBackgroundColor(Color.GRAY)
-                setPadding(1.toPx, 1.toPx, 1.toPx, 1.toPx)
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+                background = ContextCompat.getDrawable(context, android.R.drawable.picture_frame)
             }
             layoutParams = LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
             addView(LinearLayout(context).apply {
                 orientation = LinearLayout.VERTICAL
-                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-                    setBackgroundColor(Color.WHITE)
-                }
                 popupItems.forEach { item ->
                     addView(PopupItemView(context, item).apply {
                         setOnClickListener {
